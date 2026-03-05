ea30b0474ddf1dba73d593640a473e3bb2ea0af6
dor-chaouat <dorch@wix.com>
2026-03-05 13:00:20 +0200

    Initial commit from Create Wix App

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import wix from "@wix/astro";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+import cloudProviderFetchAdapter from "@wix/cloud-provider-fetch-adapter";
+const isBuild = process.env.NODE_ENV == "production";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  integrations: [wix(), react()],
+
+  image: {
+    domains: ["static.wixstatic.com"],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  ...(isBuild && { adapter: cloudProviderFetchAdapter({}) }),
+});
